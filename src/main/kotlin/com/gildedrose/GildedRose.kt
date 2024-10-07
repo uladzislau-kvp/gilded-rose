a8173337060ada4a9a3aec2b7b4cb5d880a885bf
Uladzislau Karaneuski <karaneuski.uladzislau@gmail.com>
2024-10-07 15:39:56 +0200

    'update' method refactored
    - removed redundant 'if' statements
    - replaced method logic with 'when' statement, extracted four 'when' branches by product name (type)
    - reorganized private methods order
    - fixed formatting

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,6 +9,32 @@ class GildedRose(var items: List<Item>) {
         }
     }
 
+    private fun update(item: Item) {
+        when (item.name) {
+            "Sulfuras, Hand of Ragnaros" -> {
+
+            }
+
+            "Aged Brie" -> {
+                if (item.quality < 50) item.quality += 1
+            }
+
+            "Backstage passes to a TAFKAL80ETC concert" -> {
+                if (item.quality < 50) item.quality += 1
+                if (item.sellIn <= 10) if (item.quality < 50) item.quality += 1
+                if (item.sellIn <= 5) if (item.quality < 50) item.quality += 1
+            }
+
+            else -> {
+                if (item.quality > 0) item.quality -= 1
+            }
+        }
+    }
+
+    private fun updateSellIn(item: Item) {
+        if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn -= 1
+    }
+
     private fun handleExpiredSellIn(item: Item) {
         if (item.sellIn < 0) {
             if (item.name != "Aged Brie") {
@@ -24,40 +50,6 @@ class GildedRose(var items: List<Item>) {
             } else {
                 if (item.quality < 50) {
                     item.quality += 1
-                }
-            }
-        }
-    }
-
-    private fun updateSellIn(item: Item) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn -= 1
-        }
-    }
-
-    private fun update(item: Item) {
-        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.quality -= 1
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality += 1
-
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality += 1
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality += 1
-                        }
-                    }
                 }
             }
         }
